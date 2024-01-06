d8132be5e7d85f7fec9758eaba8a0cc50ae30c66
Merbebt <103383720+mahsgit@users.noreply.github.com>
2024-01-06 12:39:27 +0300

    Added solution for Sort Colors

--- leet-code-solution/sort-colors.py
+++ leet-code-solution/sort-colors.py
@@ -3,10 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            for j in range(1,len(nums)-i):
-                if nums[j-1]>nums[j]:
-                    nums[j-1],nums[j]=nums[j],nums[j-1]
-        return nums
+        # for i in range(len(nums)):
+        #     for j in range(1,len(nums)-i):
+        #         if nums[j-1]>nums[j]:
+        #             nums[j-1],nums[j]=nums[j],nums[j-1]
+        # return nums
+
+
+
+        r, w, b = 0, 0, len(nums) - 1
+        while w <= b:
+            if nums[w] == 0:
+                nums[r], nums[w] = nums[w], nums[r]
+                r += 1
+                w += 1
+            elif nums[w] == 2:
+                nums[w], nums[b] = nums[b], nums[w]
+                b -= 1
+            else:
+                w += 1 
 
 
