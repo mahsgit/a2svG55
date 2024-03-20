00ed816592a6c77a8dc8f876f09f5ccd61382888
Merbebt <103383720+mahsgit@users.noreply.github.com>
2024-03-20 21:22:57 +0300

    Add solution for Put Marbles in Bags

--- leet-code-solution/leetcode/put-marbles-in-bags.py
+++ leet-code-solution/leetcode/put-marbles-in-bags.py
@@ -1,15 +1,21 @@
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
-        values=[]
-        prev=weights[0]
-        for val in weights[1:]:
-            prev+=val
-            values.append(prev)
-            prev=val
-        values.sort()
-        leng=len(weights)-k
-
-        return sum(values[leng:])-sum(values[:k-1])
+        # values=[]
+        # prev=weights[0]
+        # for val in weights[1:]:
+        #     prev+=val
+        #     values.append(prev)
+        #     prev=val
+        # values.sort()
+        # leng=len(weights)-k
+        # return sum(values[leng:])-sum(values[:k-1])
+        
+        values=sorted([sum(p) for p in pairwise(weights)])
+        k-=1
+        if k==0:
+             return 0
+        else:
+            return  sum(values[-k:])-sum(values[:k])
 
 
 
